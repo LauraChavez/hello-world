83e4736fd501fd04cd22dd51dc77c1c1b2425a1b
LauraChavez <laura_chavez98@hotmail.com>
2016-03-13 18:09:25 -0600

    Added files via upload

---
+++ menu.php
@@ -0,0 +1,66 @@
+
+
+<div class="navbar-wrapper">
+    <div class="container-fluid">
+        <nav class="navbar navbar-fixed-top">
+            <div class="container">               
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#" > ISA DELI & SMOKE HOUSE</a> 
+                    
+                </div>
+
+                <!--Elementos del menú-->
+
+                <div id="navbar" class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav">
+
+                        
+                        <li ><a href="#" class="">Inicio</a></li>
+
+                        <!--Catalogo-->
+
+                        <li class=" dropdown">
+                            <a href="#" class="dropdown-toggle " data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Catalogo <span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li class=" dropdown">
+                                    <a href="#" class="dropdown-toggle " data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Aves</a>
+                                </li>
+                                <li><a href="#">Carne de Res</a></li>
+                                <li><a href="#">Embutidos</a></li>
+                                <li><a href="#">Viceras</a></li>
+                            </ul>
+                        </li>
+
+                        <!--Historia-->
+
+
+                        <li><a href="#">Nuestra Historia</a></li>
+
+                        <!--Promociones-->
+
+                        <li class=" dropdown">
+                            <a href="#" class="dropdown-toggle " data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Promociones </a>
+                        </li>
+
+                    </ul>
+
+                    <!--Fuera de los apartados-->
+                    <ul class="nav navbar-nav pull-right">
+                        <li class=" dropdown">
+                            <a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Iniciar Sesión </a>
+                            
+                        </li>
+                         
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+</div>
+
